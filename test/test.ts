7f491e077d297a0abdbfd7245b65d068cba14b77
Connor Adams <connorads@users.noreply.github.com>
2020-04-26 23:59:23 +0100

    Add tests to lock and unlock a resource

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,34 @@
-test("Test", () => {
-  expect(1).toEqual(1);
+let lock = false;
+
+const execute = (command: string) => {
+  if (command[1] === "u") {
+    lock = false;
+    return "you have unlocked dev";
+  }
+
+  if (lock) {
+    return "you have already locked dev";
+  }
+  lock = true;
+  return "you have locked dev";
+};
+
+test("can lock resource", () => {
+  expect(execute("/lock dev")).toEqual("you have locked dev");
+});
+
+test("cannot lock resource twice", () => {
+  execute("/lock dev");
+  expect(execute("/lock dev")).toEqual("you have already locked dev");
+});
+
+test("can unlock resource", () => {
+  execute("/lock dev");
+  expect(execute("/unlock dev")).toEqual("you have unlocked dev");
+});
+
+test("can lock, unlock and lock resource", () => {
+  execute("/lock dev");
+  execute("/unlock dev");
+  expect(execute("/lock dev")).toEqual("you have locked dev");
 });
