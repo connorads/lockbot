f3cb766683b5905337edee01985cc4d62c5fb223
Connor Adams <connorads@users.noreply.github.com>
2020-05-03 18:08:59 +0100

    Add /locks

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,13 @@
 let lock = false;
 
 const execute = (command: string) => {
+  if (command[5] === "s") {
+    if (lock) {
+      return "dev is locked by Connor";
+    }
+    return "no active locks";
+  }
+
   if (command[1] === "u") {
     lock = false;
     return "you have unlocked dev";
@@ -12,6 +19,10 @@ const execute = (command: string) => {
   lock = true;
   return "you have locked dev";
 };
+
+beforeEach(() => {
+  lock = false;
+});
 
 test("can lock resource", () => {
   expect(execute("/lock dev")).toEqual("you have locked dev");
@@ -31,4 +42,13 @@ test("can lock, unlock and lock resource", () => {
   execute("/lock dev");
   execute("/unlock dev");
   expect(execute("/lock dev")).toEqual("you have locked dev");
+});
+
+test("can list locks when no locks", () => {
+  expect(execute("/locks")).toEqual("no active locks");
+});
+
+test("can list locks one lock exists", () => {
+  execute("/lock dev");
+  expect(execute("/locks")).toEqual("dev is locked by Connor");
 });
