f9e68d2de9aacd037f0596d0625d3acf74cb44bd
Connor Adams <connorads@users.noreply.github.com>
2020-05-03 19:51:36 +0100

    Support locking different resources

--- test/test.ts
+++ test/test.ts
@@ -1,41 +1,60 @@
-let lock = false;
+const locks: Set<string> = new Set();
 
 const execute = (command: string) => {
+  const resource = command.split(" ")[1];
+
   if (command[5] === "s") {
-    if (lock) {
-      return "dev is locked by Connor";
+    if (locks.size === 0) {
+      return "no active locks";
     }
-    return "no active locks";
+
+    let locksMessage = "";
+    locks.forEach((_, res) => {
+      locksMessage += `${res} is locked by Connor\n`;
+    });
+    return locksMessage.trimEnd();
   }
 
-  if (command[1] === "u") {
-    lock = false;
-    return "you have unlocked dev";
+  if (command[1] === "u" && resource) {
+    locks.delete(resource);
+    return `you have unlocked ${resource}`;
   }
 
-  if (lock) {
-    return "you have already locked dev";
+  if (locks.has(resource)) {
+    return `you have already locked ${resource}`;
   }
-  lock = true;
-  return "you have locked dev";
+
+  locks.add(resource);
+  return `you have locked ${resource}`;
 };
 
 beforeEach(() => {
-  lock = false;
+  locks.clear();
 });
 
 test("can lock resource", () => {
   expect(execute("/lock dev")).toEqual("you have locked dev");
+});
+test("can lock different resource", () => {
+  expect(execute("/lock test")).toEqual("you have locked test");
 });
 
 test("cannot lock resource twice", () => {
   execute("/lock dev");
   expect(execute("/lock dev")).toEqual("you have already locked dev");
 });
+test("cannot lock different resource twice", () => {
+  execute("/lock test");
+  expect(execute("/lock test")).toEqual("you have already locked test");
+});
 
 test("can unlock resource", () => {
   execute("/lock dev");
   expect(execute("/unlock dev")).toEqual("you have unlocked dev");
+});
+test("can unlock different resource", () => {
+  execute("/lock test");
+  expect(execute("/unlock test")).toEqual("you have unlocked test");
 });
 
 test("can lock, unlock and lock resource", () => {
@@ -51,4 +70,12 @@ test("can list locks when no locks", () => {
 test("can list locks one lock exists", () => {
   execute("/lock dev");
   expect(execute("/locks")).toEqual("dev is locked by Connor");
+});
+
+test("can list locks multiple locks", () => {
+  execute("/lock dev");
+  execute("/lock test");
+  expect(execute("/locks")).toEqual(
+    "dev is locked by Connor\ntest is locked by Connor"
+  );
 });
