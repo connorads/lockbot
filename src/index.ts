781badbbbb772339bcafecac5aad6545658c85c9
Connor Adams <connorads@users.noreply.github.com>
2020-05-19 00:06:05 +0100

    Send lock and unlock responses to channel 📢 Other responses can only be seen by the user 📩

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,37 @@ import {
   Logger,
   AckFn,
   RespondArguments,
+  SayFn,
 } from "@slack/bolt";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import * as awsServerlessExpress from "aws-serverless-express";
 import * as env from "env-var";
-import LockBot from "./lock-bot";
+import LockBot, { Response } from "./lock-bot";
 import DynamoDBLockRepo from "./dynamodb-lock-repo";
+
+const respond = async (
+  logger: Logger,
+  ack: AckFn<string | RespondArguments>,
+  say: SayFn,
+  responsePromise: Promise<Response>
+): Promise<void> => {
+  try {
+    const response = await responsePromise;
+    switch (response.destination) {
+      case "channel":
+        await say(response.message);
+        await ack();
+        break;
+      case "user":
+        await ack(response.message);
+        break;
+      default:
+    }
+  } catch (e) {
+    logger.error(e);
+    await ack(`❌ Unhandled error 😢`);
+  }
+};
 
 const expressReceiver = new ExpressReceiver({
   signingSecret: env.get("SLACK_SIGNING_SECRET").required().asString(),
@@ -21,38 +46,18 @@ const app = new App({
   processBeforeResponse: true,
 });
 
-const run = async (
-  logger: Logger,
-  ack: AckFn<string | RespondArguments>,
-  fn: () => Promise<void>
-): Promise<void> => {
-  try {
-    await fn();
-  } catch (e) {
-    logger.error(e);
-    await ack(`:x: Failed to post a message (error: ${e})`);
-  }
-};
-
 const lockBot = new LockBot(new DynamoDBLockRepo(new DocumentClient()));
-app.command("/locks", async ({ logger, ack }) => {
-  await run(logger, ack, async () => {
-    const message = await lockBot.locks();
-    await ack(message);
-  });
+app.command("/locks", async ({ logger, ack, say }) => {
+  const responsePromise = lockBot.locks();
+  await respond(logger, ack, say, responsePromise);
 });
 app.command("/lock", async ({ command, logger, ack, say }) => {
-  await run(logger, ack, async () => {
-    const message = await lockBot.lock(command.text, command.user_name);
-    await say(message);
-    await ack();
-  });
+  const responsePromise = lockBot.lock(command.text, command.user_name);
+  await respond(logger, ack, say, responsePromise);
 });
 app.command("/unlock", async ({ command, logger, ack, say }) => {
-  await run(logger, ack, async () => {
-    await say(await lockBot.unlock(command.text, command.user_name));
-    await ack();
-  });
+  const responsePromise = lockBot.unlock(command.text, command.user_name);
+  await respond(logger, ack, say, responsePromise);
 });
 
 const server = awsServerlessExpress.createServer(expressReceiver.app);
