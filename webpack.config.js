48a781413977d037ecc68feed1d3b1ea183eb5af
Connor Adams <connorads@users.noreply.github.com>
2020-05-29 23:19:03 +0100

    Use serverless-webpack for faster deploy 🚀

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const slsw = require("serverless-webpack");
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  entry: slsw.lib.entries,
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  output: {
+    libraryTarget: "commonjs",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js",
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+};
