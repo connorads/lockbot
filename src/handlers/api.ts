31198b482ee505b7fe52d79fff9c5856c976e29e
Connor Adams <connorads@users.noreply.github.com>
2020-08-15 00:42:10 +0100

    Use lock instead of resource. Add authorize logging. Remove team and channel logging.

--- src/handlers/api.ts
+++ src/handlers/api.ts
@@ -45,9 +45,10 @@ const authorizer = async (req: Request, res: Response, next: NextFunction) => {
   } else if (
     await tokenAuthorizer.isAuthorized(user.pass, user.name, channel, team)
   ) {
+    console.log("Authorized", { team, channel, username: user.name });
     next();
   } else {
-    console.log("Unauthorized", { channel, team, user: user.name });
+    console.log("Unauthorized", { team, channel, username: user.name });
     res.status(401).json({ error: "Unauthorized" });
   }
 };
@@ -56,28 +57,28 @@ app.get(
   "/api/teams/:team/channels/:channel/locks",
   authorizer,
   async (req, res) => {
-    const { channel, team } = req.params;
+    const { team, channel } = req.params;
     const locksMap = await lockRepo.getAll(channel, team);
     const locks: Lock[] = [];
     locksMap.forEach((v, k) => locks.push({ name: k, owner: v }));
-    console.log("Retrieved locks", { channel, team, locks });
+    console.log("Retrieved locks", { locks });
     res.status(200).json(locks);
   }
 );
 
 app.get(
-  "/api/teams/:team/channels/:channel/locks/:resource",
+  "/api/teams/:team/channels/:channel/locks/:lock",
   authorizer,
   async (req, res) => {
-    const { channel, team, resource } = req.params;
-    const lockOwner = await lockRepo.getOwner(resource, channel, team);
+    const { team, channel, lock: lockName } = req.params;
+    const lockOwner = await lockRepo.getOwner(lockName, channel, team);
     if (lockOwner) {
-      const lock: Lock = { name: resource, owner: lockOwner };
-      console.log("Retrieved lock", { channel, team, lock });
+      const lock: Lock = { name: lockName, owner: lockOwner };
+      console.log("Retrieved lock", { lock });
       res.status(200).json(lock);
     } else {
-      console.log("Lock not found", { channel, team, resource });
-      res.status(404).json({ error: `${resource} not found` });
+      console.log("Lock not found", { lockName });
+      res.status(404).json({ error: `${lockName} not found` });
     }
   }
 );
@@ -103,13 +104,13 @@ app.post(
     const lockOwner = await lockRepo.getOwner(lock.name, channel, team);
     if (!lockOwner) {
       await lockRepo.setOwner(lock.name, lock.owner, channel, team);
-      console.log("Added lock", { channel, team, lock });
+      console.log("Added lock", { lock });
       res.status(201).json(lock);
     } else if (lockOwner === lock.owner) {
-      console.log("Lock exists", { channel, team, lock });
+      console.log("Lock exists", { lock });
       res.status(200).json(lock);
     } else {
-      console.log("Cannot lock", { channel, team, lock, lockOwner });
+      console.log("Cannot lock", { lock, lockOwner });
       res
         .status(403)
         .json({ error: `${lock.name} is already locked by ${lockOwner}` });
